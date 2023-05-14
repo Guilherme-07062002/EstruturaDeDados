0fd5dd289afe8508385e81469a842fdb85590b52
Guilherme-07062002 <guilherme762002@gmail.com>
2023-05-14 17:23:08 -0300

    add comentario

--- testeChrono/teste.cpp
+++ testeChrono/teste.cpp
@@ -11,8 +11,11 @@ int main(){
 
     // Armazena tempo do fim da execução
     auto end = std::chrono::high_resolution_clock::now();
+
     // Calcula a diferença de tempo entre início e fim e converte em segundos.
     std::chrono::duration<double , std::milli> diff = end - start;
+
+    // Exibe no terminal
     std::cout << "Tempo de execução: " << diff.count() << " milissegundos\n";
     
     return 0;
